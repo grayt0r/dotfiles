7ff47d4950af151ece4d6cd0e1f246b5f319b4bd
Ross Grayton <rossgrayton@gmail.com>
2018-12-10 23:18:53 +0000

    Add iTerm2 config

--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ ln -sf $(pwd)/git/.gitignore_global ~/.gitignore_global
 
 # Setup global gitignore
 git config --global core.excludesfile ~/.gitignore_global
+
+# Setup iTerm2
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
